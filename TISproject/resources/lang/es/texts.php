44730e0a97f1298452b5c6e99e79b1dbd330bb21
your username <tmarina@eafit.edu.co>
2023-03-11 16:12:26 -0500

    implementación de Lang

---
+++ TISproject/resources/lang/es/texts.php
@@ -0,0 +1,13 @@
+<?php
+return [
+  #Tomas Marin
+  'namePage'=>"Point 'n shoot",
+  'orderByPrice' => 'Ordenar por precio',
+  'textAboutUs1' => 'Bienvenido a nuestra sección de inicio, donde mostramos una gran variedad de artículos fotográficos que ayudaran a proyectar su imaginación e inspirarán su creatividad. Tanto si eres un fotógrafo experimentado como si acabas de empezar en el mundo de la fotografía, tenemos algo para ti.',
+  'textAboutUs2' => 'Nuestra colección incluye cámaras, objetivos, trípodes, equipos de iluminación y otros accesorios de las mejores marcas del sector. Nos enorgullecemos de ofrecer sólo productos de la más alta calidad, por lo que puede estar seguro de que está recibiendo lo mejor de lo mejor.',
+  'textAboutUs3' => 'Además de nuestra oferta de productos, también proporcionamos servicio de revelado de rollos 35mm y 120, para los amantes a la fotografía análoga.',
+  'textAboutUs4' => 'Tanto si eres un fotógrafo profesional que quiere mejorar su equipo como si eres un aficionado que quiereampliar sus conocimientos, estamos aquí para ayudarte. Revisa nuestra pagina y descubre las posibilidades de lo que puedes crear con nuestros artículos fotográficos.'
+  #Simon Cardenas 
+
+  #Juan Pablo Yepes
+];
--- TISproject/resources/views/home/about.blade.php
+++ TISproject/resources/views/home/about.blade.php
@@ -2,25 +2,18 @@
 @section('title', "Point 'n Shoot")
 @section('content')
 <div class="divContenidoPaginaSobreNosotros">
-  <h1 id="titleAboutUs">Point 'n shoot</h1>
+  <h1 id="titleAboutUs">{{ __('texts.namePage') }}</h1>
   <p id="paragraphPageAboutUs">
-  Bienvenido a nuestra sección de inicio, donde mostramos una gran variedad de artículos fotográficos 
-  que ayudaran a proyectar su imaginación e inspirarán su creatividad. Tanto si eres un fotógrafo 
-  experimentado como si acabas de empezar en el mundo de la fotografía, tenemos algo para ti.
+    {{ __('texts.textAboutUs1') }}
   <br>
   <br>
-  Nuestra colección incluye cámaras, objetivos, trípodes, equipos de iluminación y otros accesorios de las 
-  mejores marcas del sector. Nos enorgullecemos de ofrecer sólo productos de la más alta calidad, 
-  por lo que puede estar seguro de que está recibiendo lo mejor de lo mejor.
+    {{ __('texts.textAboutUs2') }}
   <br>
   <br>
-  Además de nuestra oferta de productos, también proporcionamos servicio de revelado de rollos 35mm y 120, 
-  para los amantes a la fotografía análoga.
+    {{ __('texts.textAboutUs3') }}
   <br>
   <br>
-  Tanto si eres un fotógrafo profesional que quiere mejorar su equipo como si eres un aficionado que quiere
-  ampliar sus conocimientos, estamos aquí para ayudarte. Revisa nuestra pagina y descubre las posibilidades
-  de lo que puedes crear con nuestros artículos fotográficos.
+    {{ __('texts.textAboutUs4') }}
   </p>
 </div>
 @endsection
